2d1e0f2a171bbd1e721042f6a511b5aaa7eac4db
SAC819 <shaniece_cole@yahoo.com>
2017-09-27 12:55:48 -0500

    initial

---
+++ main.js
@@ -0,0 +1,10 @@
+friends= ['Cam', 'Sierra', 'Shakayla', 'JaKayla','Sheray'];
+
+console.log(friends);
+
+
+
+for (let i = 0; i<friends.length; i++) {
+    for(let s=99; s>0; s-- )
+    console.log(s+" lines of code in the file,"+s+"lines of code;"+ friends[i] + "strikes one out, clears it all out, ");
+  }
